5fb5c17551179a4d8770d638f547712386013c82
vladuhalys <vlad.lysenko1999@gmail.com>
2022-10-30 22:24:34 +0200

    Update base class Add new class Android

---
+++ android_system.cpp
@@ -0,0 +1,5 @@
+#include "android_system.h"
+
+string Android::Operation() {
+    return "Start OS(" + Name + " " + Version + "). Check Google Play!";
+}
--- android_system.h
+++ android_system.h
@@ -1,6 +1,6 @@
 #include "operating_system.h"
 
-class IOS : public OS {
+class Android : public OS {
 public:
     string Operation() override;
 };
